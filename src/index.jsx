df58c75e2b2bdfef640c0e79db817a22eecacd20
Muki <mukidev881@gmail.com>
2024-12-25 16:19:51 +0100

    use Link

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './pages/Home';
 import Survey from './pages/Survey';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/survey" element={<Survey />}/>
