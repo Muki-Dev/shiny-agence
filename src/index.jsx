a3827e3a65539f893d2364855586fa1a6c7c10a0
Muki <mukidev881@gmail.com>
2025-01-20 10:33:54 +0100

    style global

--- src/index.jsx
+++ src/index.jsx
@@ -9,14 +9,19 @@ import Freelance from './pages/Freelance';
 import Results from './pages/Results';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { createGlobalStyle } from 'styled-components';
 
-
-
+const GlobalStyle = createGlobalStyle`
+  div {
+        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <Header />
       <Routes>
         <Route path="/" element={<Home />}/>
