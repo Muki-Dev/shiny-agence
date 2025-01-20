645cdcef36b6ba600cfa38ecd91467b3efd34a3e
Muki <mukidev881@gmail.com>
2025-01-20 11:17:10 +0100

    error style

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,8 +1,35 @@
+import styled from "styled-components";
+import colors from "../../utils/style/colors";
+
+const ErrorWrapper = styled.div`
+    margin: 30px;
+    display: flex;
+    flex-direction: column;
+    background-color: ${colors.backgroundLight};
+    align-items: center;
+`
+
+const ErrorTitle = styled.h1`
+    font-weight: 300:
+`
+
+const ErrorSubTitle = styled.h2`
+    font-weight: 300;
+    colors: ${colors.secondary}
+`
+const Illustration = styled.img`
+    max-width: 800px;
+`
 function Error(){
     return(
-        <div>
-             <h1>Oups 🙈 Cette page n'existe pas</h1>
-        </div>
+        <ErrorWrapper>
+             <ErrorTitle>Oups...</ErrorTitle>
+             <Illustration src="https://github.com/atoulmet/assets/blob/master/404.svg?raw=true" />
+             <ErrorSubTitle>
+                 Il semblerait que la page que vous cherchez n’existe pas
+             </ErrorSubTitle>
+
+        </ErrorWrapper>
     )
 }
 
