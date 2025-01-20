8c39ebdd8ff57eae75f56231c5f9ed9f1075143b
Muki <mukidev881@gmail.com>
2025-01-20 09:47:23 +0100

    logique styled components

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,23 @@
+import styled from 'styled-components';
 import React from 'react';
 import PropTypes from 'prop-types'
 import DefaultPicture from '../../assets/profile.png'
 
 function Card({ label,title,picture }){
+    const CardLabel = styled.span`
+        color: #5843e4;
+        font-size: 22px;
+        font-weight: bold;
+    `
+    const CardImage = styled.img`
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    `
     return(
         <div style={{display:'flex', flexDirection:'column', padding: 15}}>
-            <span>{label}</span>
-            <img src={picture} alt='freelance' width={80} height={80} />
+            <CardLabel>{label}</CardLabel>
+            <CardImage src={picture} alt='freelance' />
             <span>{title}</span>
         </div>
     )
