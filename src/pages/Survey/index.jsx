32b382ba6dac82166f63d6e8bb67647deba3413d
Muki <mukidev881@gmail.com>
2025-01-22 22:38:50 +0100

    update surveyQuestion

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,4 +1,32 @@
 import { Link, useParams } from "react-router-dom";
+import styled from "styled-components";
+import colors from "../../utils/style/colors";
+import { Loader } from "../../utils/style/Atoms";
+
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+`
+
+const QuestionContent = styled.span`
+  margin: 30px;
+`
+
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: black;
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`
 
 function Survey(){
     const { questionNumber } = useParams();
@@ -7,16 +35,17 @@ function Survey(){
     const nextQuestionNumber = questionNumberInt + 1;
    
     return(
-        <div>
-            <h1>Questionnaire🧮</h1>
-            <h2>Question { questionNumber }</h2>
-            <Link to={`/survey/${prevQuestionNumber}`}>Precedent</Link>
-            {
-                questionNumberInt === 10 ? (
-                    <Link to="/results">Results</Link>
-                ):(<Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>)
-            }
-        </div>
+        <SurveyContainer>
+            <QuestionTitle>Question { questionNumber }</QuestionTitle>
+                <LinkWrapper>
+                    <Link to={`/survey/${prevQuestionNumber}`}>Precedent</Link>
+                    {
+                        questionNumberInt === 10 ? (
+                            <Link to="/results">Results</Link>
+                        ):(<Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>)
+                    }
+                </LinkWrapper>
+        </SurveyContainer>
     )
 }
 
