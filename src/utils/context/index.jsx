92fc73ba648a7d226053c9f0262275f373c04439
Muki <mukidev881@gmail.com>
2025-02-07 14:45:02 +0100

    survey context

--- src/utils/context/index.jsx
+++ src/utils/context/index.jsx
@@ -13,3 +13,19 @@ export const ThemeProvider = ({ children }) => {
         </ThemeContext.Provider>
     )
 }
+
+export const SurveyContext = createContext();
+
+export const SurveyProvider = ({children}) => {
+    const [answers,setAnswers] = useState({});
+
+    const saveAnswer = (newAnswer) => {
+        setAnswers({...answers, ...newAnswer})
+    }
+    
+    return(
+        <SurveyContext.Provider value={{ answers, saveAnswer}}>
+            {children}
+        </SurveyContext.Provider>
+    )
+}
